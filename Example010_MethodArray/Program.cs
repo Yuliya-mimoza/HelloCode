5e1182e98dec80783dc1e99994dd4a37691ce918
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-18 14:26:40 +0400

    Example010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа, поиска заданного элемента в массиве
+int[] array = { 1, 29, 31, 18, 4, 53, 62, 7, 18 };
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
