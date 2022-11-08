6188ffbaf2d166069f289aaecc93aa12d3c5ec47
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-08 23:42:31 +0400

    Example013_Text

--- Example013_Text/Program.cs
+++ Example013_Text/Program.cs
@@ -1,7 +1,6 @@
 ﻿// ===Работа с текстом
 // Дан текст. В тексте нужно все пробелы заменить черточками,
 // Маленькие буквы "к", заменить большими "К".
-// А большие "С" заменить маленькими "с".
 
 string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
             + "ежели бы вас послали вместо нашего милого Винценгероде, "
@@ -21,13 +20,9 @@ string Replace(string text, char oldValue, char newValue)
     return result;
 }
 string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
+Console.WriteLine(newText); 
 Console.WriteLine();
 
 newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-
-newText = Replace(newText, 'С', 'с');
 Console.WriteLine(newText);
 Console.WriteLine();
