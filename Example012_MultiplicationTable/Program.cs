5c3577df600d4832d8a0217b9cca13842733559d
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-08 19:23:23 +0400

    Example012_MultiplicationTable

---
+++ Example012_MultiplicationTable/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вид 4 Таблица умножения (что-то принимает, что-то возвращает) через цикл FOR
+    string result = String.Empty; // равно пустой строке
+    for (int i=2; i<=10; i++)
+    {
+        for (int j=2; j<=10; j++)
+        {
+            Console.WriteLine($"{i}*{j}={i*j}");
+        }
+        Console.WriteLine();
+    }
