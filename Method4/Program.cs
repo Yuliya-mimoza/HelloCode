b8fefe0323a82d77aabe431e99dc1df2fb929eed
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-08 18:44:36 +0400

    вид4

---
+++ Method4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вид 4 (что-то принимает, что-то возвращает)
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // равно пустой строке
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+// Вызов
+string res=Method4(10, "asdf");
+Console.WriteLine(res);
