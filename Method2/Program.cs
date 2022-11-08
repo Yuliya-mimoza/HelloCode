a9b962e3373938df4b1edcda1e0feb4cc396e180
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-08 17:37:39 +0400

    вид2

---
+++ Method2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вид2
+/*void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Вызов
+Method2("Текст сообщения");*/
+
+
+
+
+// Вид21
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Вызов
+//Method21("Текст сообщения", 4);
+Method21(count:4,msg:"Текст"); // конкретно прописываем для переменных
