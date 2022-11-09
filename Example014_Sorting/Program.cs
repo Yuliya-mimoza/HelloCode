b6fa4852b9466db24a9451bfc034d651d2afdc93
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-09 19:29:19 +0400

    Example014_Sorting

---
+++ Example014_Sorting/Program.cs
@@ -0,0 +1,36 @@
+﻿// 
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array) // вывод массива
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // сортировка в массиве
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length - 1; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
